133d447c5f5d527ae395545d0f4a7e9d1ecc4377
Leo <levan2312@yandex.ru>
2023-07-03 01:03:12 +0400

    final commit

--- src/test/java/ru/netology/task/TodosTest.java
+++ src/test/java/ru/netology/task/TodosTest.java
@@ -32,17 +32,17 @@ public class TodosTest {
     @Test
     public void shouldReturnMatchingTasksForQuery() {
         SimpleTask simpleTask = new SimpleTask(1, "Позвонить родителям");
-        Epic epic = new Epic(2, new String[]{"Подготовить отчет", "Провести совещание"});
-        Meeting meeting = new Meeting(3, "Планирование проекта", "Проект X", "2023-05-20");
+        Epic epic = new Epic(2, new String[]{"Подготовить презентацию", "Собрать данные", "Написать отчет"});
+        Meeting meeting = new Meeting(3, "Выкатка 3й версии приложения", "Приложение НетоБанка", "Во вторник после обеда");
 
         Todos todos = new Todos();
+
         todos.add(simpleTask);
         todos.add(epic);
         todos.add(meeting);
 
-        Task[] expected = { epic, meeting };
-        Task[] actual = todos.search("планирование");
-
+        Task[] expected = {epic};
+        Task[] actual = todos.search("Подготовить");
         Assertions.assertArrayEquals(expected, actual);
     }
 }
